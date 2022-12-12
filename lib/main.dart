bcdbaafa3e2738c110323df6f3f1adae6ef9077f
ajPetrov <antoan.baraban@live.com>
2022-12-12 21:28:17 +0200

    Migrate the buttons to a separated class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'answer.dart';
 import './question.dart';
 
 void main() {
@@ -37,18 +37,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(questions[_questionIndex]),
-            ElevatedButton(
-                onPressed: () => _answerQuestion(),
-                child: Text('Answer 1')
-            ),
-            ElevatedButton(
-                onPressed: () => _answerQuestion(),
-                child: Text('Answer 2')
-            ),
-            ElevatedButton(
-                onPressed: () => _answerQuestion(),
-                child: Text('Answer 3')
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],),
       ),
     );
