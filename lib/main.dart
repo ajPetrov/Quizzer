d53504428d9fc2dd62863f190613bfc95991ca8d
ajPetrov <antoan.baraban@live.com>
2022-12-13 22:01:27 +0200

    Add a quiz reset button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quiz.dart';
+import 'result.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,20 +17,45 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 15},
+        {'text': 'Red', 'score': 10},
+        {'text': 'Green', 'score': 5},
+        {'text': 'White', 'score': 2}
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal',
-      'answers': ['Rabbit', 'Snake', 'Hippo', 'Tiger'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 25},
+        {'text': 'Hippo', 'score': 5},
+        {'text': 'Tiger', 'score': 15}
+      ],
     },
     {
       'questionText': 'Which is your favourite Harry Potter house?',
-      'answers': ['Gryffindor ', 'Slytherin', 'Ravenclaw', 'Hufflepuff'],
+      'answers': [
+        {'text': 'Gryffindor', 'score': 10},
+        {'text': 'Slytherin', 'score': 30},
+        {'text': 'Ravenclaw', 'score': 25},
+        {'text': 'Hufflepuff', 'score': 5}
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -43,7 +69,7 @@ class _MyAppState extends State<MyApp> {
           title: Text('Quizzer app'),
       ),
         body: _questionIndex < _questions.length ? Quiz(questions: _questions, answerQuestion: _answerQuestion, questionIndex: _questionIndex)
-            : Center(child: Text('You finished the quiz'),),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase{
+    var resultText;
+    if (resultScore <= 15){
+      resultText = 'Low Score but still you good';
+    } else if (resultScore <= 20){
+      resultText = 'I guess you are alright';
+    } else if (resultScore <= 30){
+      resultText = 'Pretty handsome I would say';
+    } else{
+      resultText = "Oh lala";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('You finished the quiz'),);
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Restart Quiz!'),
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
   }
 }
